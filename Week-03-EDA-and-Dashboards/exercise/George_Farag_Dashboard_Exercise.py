32f462d0d3eece60a66dac6fb56d8a86e1b91ada
George Farag <georgefarag28@gmail.com>
2025-09-18 03:56:04 +0000

    added a few things

--- Week-03-EDA-and-Dashboards/exercise/George_Farag_Dashboard_Exercise.py
+++ Week-03-EDA-and-Dashboards/exercise/George_Farag_Dashboard_Exercise.py
@@ -8,21 +8,35 @@ data = pd.read_csv("/workspaces/ds-fall-2025-fri-1230/Week-03-EDA-and-Dashboards
 #1. What's the breakdown of genres for the movies that were rated?
 #get the data that has ratings only
 rated_data = data.dropna(subset=["rating"])
-genre_count = rated_data["genres"].value_counts(normalize=True)*100
+genre_count = rated_data["genres"].value_counts(normalize=True) * 100
 genre_df = genre_count.reset_index()
 genre_df.columns = ["Genre", "Percentage"]
 
-st.write("### Genre Breakdown")
 fig = px.bar(
     genre_df,
     x="Genre",
-    y="Percentage",
-    text=genre_df["Percentage"].map(lambda x: f"{x:.2f}%"),
-    title="Movie Genres (Rated Only)"
+    y="Percentage",  
+    text=genre_df["Percentage"].map(lambda x: f"{x:.3f}%"),
+    title="Rating per movie genre",
+    color_discrete_sequence=["#199201"]
 )
 fig.update_traces(textposition="outside")
 st.plotly_chart(fig)
+
 #2. Which genres have the highest viewer satisfaction (highest ratings)? 
-st.write("Average Ratings by Genre")
-average_rating = rated_data.groupby("genres")["rating"].mean()
-st.write(average_rating)
+avg_rating = rated_data.groupby("genres")["rating"].mean().round(2).reset_index()
+avg_rating.columns = ["Genre", "Average Rating"]
+
+fig = px.bar(
+    avg_rating.sort_values("Average Rating", ascending=False),
+    x="Genre",
+    y="Average Rating",
+    text="Average Rating",
+    title="Average Viewer Rating per Genre",
+    color="Average Rating",
+    color_continuous_scale="Blues",
+    height = 500
+)
+
+fig.update_traces(textposition="outside")
+st.plotly_chart(fig)
